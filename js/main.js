2cf45ccc9ee313aec5acd75d7c2730a6d46ba6b6
Aida Bevrnja <73084714+aidab394@users.noreply.github.com>
2021-09-14 23:15:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -118,7 +118,7 @@
 	// Click outside of offcanvass
 	var mobileMenuOutsideClick = function() {
 
-		$(document).click(function (e) {
+		/*$(document).click(function (e) {
 	    var container = $("#colorlib-aside, .js-colorlib-nav-toggle");
 	    if (!container.is(e.target) && container.has(e.target).length === 0) {
 
@@ -130,16 +130,16 @@
 	    	}
 	    	
 	    }
-		});
+		});*/
 
-		$(window).scroll(function(){
+		/*$(window).scroll(function(){
 			if ( $('body').hasClass('offcanvas') ) {
 
     			$('body').removeClass('offcanvas');
     			$('.js-colorlib-nav-toggle').removeClass('active');
 			
 	    	}
-		});
+		});*/
 
 	};
 
